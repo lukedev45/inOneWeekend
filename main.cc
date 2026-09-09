da9595ad8e8ea69947cb0aea0f0fac0058b0751d
Luke Devlin <lukedevlin45@gmail.com>
2026-09-09 17:59:27 +0100

    Add sphere to image

--- main.cc
+++ main.cc
@@ -1,10 +1,22 @@
 #include "colour.h"
 #include "ray.h"
 #include "vec3.h"
-
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  // Boils down to a quadratic equation
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2.0 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant >= 0);
+}
+
 colour ray_colour(const ray& r) {
+  if (hit_sphere(point3(0,0,-1), 0.5, r))
+    return colour(1,0,0);
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0);
